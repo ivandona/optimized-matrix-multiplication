042b91bbd63013c954285d3c991cc5b8f9d45121
Donà <ivan.dona@hpc-head-n1.unitn.it>
2025-07-08 01:07:06 +0200

    cleanup

--- scripts/omp_base.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#PBS -l select=1:ncpus=4:mem=1gb
-#PBS -l walltime=00:30:00
-#PBS -q short_cpuQ
-#PBS -o logs/outputs/omp_base_o.txt
-#PBS -e logs/outputs/omp_base_e.txt
-
-./optimized-matrix-multiplication/build/bin/algorithms/base/omp_base 1024 4
